df88bef72009d8ce631d9ab232d461c2e758e98a
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2024-11-03 21:35:15 -0800

    properly cleaned text

--- file_conversion.py
+++ file_conversion.py
@@ -1,10 +1,10 @@
 import json
-import email
 import os
 from email import policy
 from email.parser import BytesParser
+from bs4 import BeautifulSoup
+import re
 
-from sympy.physics.vector import outer
 
 
 def eml_to_json(eml_file_path):
@@ -52,12 +52,26 @@ def eml_to_json(eml_file_path):
                 }
                 email_data["attachments"].append(attachment)
     else:
-        email_data["body"] = msg.get_payload(decode=True).decode(decoding, errors="replace")
+        if msg.get_content_type() == "text/plain":
+            email_data["body"] = msg.get_payload(decode=True).decode(decoding, errors="replace")
+        elif msg.get_content_type() == "text/html":
+            html_content = msg.get_payload(decode=True).decode(decoding, errors="replace")
+            soup = BeautifulSoup(html_content, "html.parser")
+            email_data["body"] = soup.get_text(separator="\n").strip()
 
     # Convert the email data to JSON
     email_json = json.dumps(email_data, indent=4)
     return email_json
 
+def clean_text(text):
+    # print(f"checking {filename}")
+    text = text.encode('latin1', errors='ignore').decode('utf-8', errors='ignore')
+    cleaned_text = re.sub(r'\\u[0-9A-Fa-f]{4}', '', text)
+    if '<html>' in cleaned_text:
+        soup = BeautifulSoup(cleaned_text, 'html.parser')
+        cleaned_text = soup.get_text()
+        # title = soup.title.string if soup.title else 'No title'
+    return cleaned_text
 
 def convert_all_to_json():
     input_folder = "emails"
@@ -68,16 +82,24 @@ def convert_all_to_json():
         file_path = os.path.join(input_folder, filename)
         if filename.endswith(".eml") and os.path.isfile(file_path):
             email_json = eml_to_json(file_path)
+            data = json.loads(email_json)
+            data['body'] = clean_text(data['body'])
+            data['subject'] = clean_text(data['subject'])
+            email_json = json.dumps(data, indent=4)
+
             output_filename = f"{os.path.splitext(filename)[0]}.json"
             output_path = os.path.join(output_folder, output_filename)
             with open(output_path, "w", encoding="utf-8") as json_file:
                 json_file.write(email_json)
-
             print(f"Saved JSON for {filename} to {output_filename}")
 
-if __name__ == "__main__":
-    eml_file_path = "./id000.eml"
-    email_json = eml_to_json(eml_file_path)
-    print(email_json)
 
+if __name__ == "__main__":
+    # eml_file_path = "./id000.eml"
+    # email_json = eml_to_json(eml_file_path)
+    # print(email_json)
     convert_all_to_json()
+
+    # with open(os.path.join('email_jsons','id640.json'), "r", encoding="utf-8") as json_file:
+    #     data = json.load(json_file)
+    #     print(data['body'])
